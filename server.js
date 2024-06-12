54f9ed00e4fe49c902a7dcdc00f0d7c80df86e4d
Varadut1 <varaduttarwar535@gmail.com>
2024-06-12 10:22:00 +0530

    deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 })
