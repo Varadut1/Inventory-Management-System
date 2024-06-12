677d942d311438fce75420ca40b04f0dd257355e
Varadut1 <varaduttarwar535@gmail.com>
2024-06-12 20:20:16 +0530

    fixed bug of undefined

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -6,37 +6,37 @@ const catchAsync = require('../utils/catchAsync');
 
 const signToken = customerId => {
     return jwt.sign({ customerId }, process.env.SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN
+        expiresIn: process.env.JWT_EXPIRES_IN
     });
-  };
+};
 
-  
 const createSendToken = (customer, statusCode, res) => {
     const token = signToken(customer.customerId);
-    customer.password = undefined;
+    let copycustomer = { ...customer }
+    copycustomer.password = undefined;
     res.status(statusCode).json({
-      status: 'success',
-      token,
-      data: {
-        customer
-      }
+        status: 'success',
+        token,
+        data: {
+          copycustomer
+        }
     });
-  };
+};
 
-exports.signup = catchAsync(async(req, res, next) => {
+exports.signup = catchAsync(async (req, res, next) => {
     const { name, email, password, role } = req.body;
     if (!email || !password || !name) {
-      const err = new AppError('Please provide email, password and name!', 400);
-      return err.transfer(res);
+        const err = new AppError('Please provide email, password and name!', 400);
+        return err.transfer(res);
     }
-    if(role){
-      const err = new AppError('You are not authorized to set roles here', 400);
-      return err.transfer(res);
+    if (role) {
+        const err = new AppError('You are not authorized to set roles here', 400);
+        return err.transfer(res);
     }
     const customer = await Customer.createCustomer(name, email, password);
-    if(!customer){
-      const err = new AppError('Cannot create new customer because customer with same Mail already exists!', 400);
-      return err.transfer(res);
+    if (!customer) {
+        const err = new AppError('Cannot create new customer because customer with same Mail already exists!', 400);
+        return err.transfer(res);
     }
     createSendToken(customer, 201, res);
 });
@@ -44,81 +44,80 @@ exports.signup = catchAsync(async(req, res, next) => {
 exports.signIn = catchAsync(async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
-      const err = new AppError('Please provide email and password!', 400);
-      return err.transfer(res);
+        const err = new AppError('Please provide email and password!', 400);
+        return err.transfer(res);
     }
     const customer = await Customer.validateCustomer(email, password);
     if (customer) {
-      createSendToken(customer, 200, res);
+        createSendToken(customer, 200, res);
     } else {
-      const err = new AppError('Authentication failed. Invalid email or password.', 400);
-      return err.transfer(res);
+        const err = new AppError('Authentication failed. Invalid email or password.', 400);
+        return err.transfer(res);
     }
 });
 
+exports.protect = catchAsync(async (req, res, next) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+    if (!token) {
+        const err = new AppError('You are not logged in! Please login again.', 401);
+        return err.transfer(res);
+    }
 
+    const decoded = await promisify(jwt.verify)(token, process.env.SECRET);
+    const freshUser = await Customer.findCustomerById(decoded.customerId);
 
-exports.protect = catchAsync(async(req, res, next) => {
-  let token;
-  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
-    token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : null
-  }
-  if(!token){
-    const err = new AppError('You are not logged in! Please login again.', 401);
-    return err.transfer(res);
-  }
-  
-  const decoded = await promisify(jwt.verify)(token, process.env.SECRET);
+    if (!freshUser) {
+        const err = new AppError('The user belonging to token no longer exists', 401);
+        return err.transfer(res);
+    }
 
-  const freshUser = await Customer.findCustomerById(decoded.customerId);
-  if(!freshUser){
-    const err = new AppError('The user belonging to token no longer exists', 401);
-    return err.transfer(res);
-  }
-  req.customer = freshUser;
-  next();
+    req.customer = freshUser;
+    next();
 });
 
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.customer.role)) {
+            const err = new AppError('You do not have permission to perform this action!', 404);
+            return err.transfer(res);
+        }
+        next();
+    }
+};
 
-exports.restrictTo = (...roles) =>{
-  return (req, res, next) => {
-      if(!roles.includes(req.customer.role)){
-        const err = new AppError('You do not have permission to perform this action!', 404)
-          return err.transfer(res);
-      }
-      next();
-  }
-}
-
-exports.getCustomer = catchAsync(async(req, res) =>{
-  const data = await Customer.getCustomers();
-  for(const key in data){
-    data[key].password = undefined;
-  }
-  if(!data){
-    const err = new AppError('No customers found', 404);
-    return err.transfer(res);
-  }
-  res.status(200).json({
-    status: 'success',
-    data
+exports.getCustomer = catchAsync(async (req, res) => {
+    const data1 = await Customer.getCustomers();
+    const data = {...data1}
+    for (const key in data) {
+        data[key].password = undefined;
+    }
+    if (!data) {
+        const err = new AppError('No customers found', 404);
+        return err.transfer(res);
+    }
+    res.status(200).json({
+        status: 'success',
+        data
     });
-})
+});
 
-exports.addAdmin = catchAsync(async(req, res)=> {
-  const { name, email, password, authority } = req.body;
+exports.addAdmin = catchAsync(async (req, res) => {
+    const { name, email, password, authority } = req.body;
     if (!email || !password || !name) {
-      const err = new AppError('Please provide email, password and name!', 400);
-      return err.transfer(res);
+        const err = new AppError('Please provide email, password and name!', 400);
+        return err.transfer(res);
     }
-    if(authority!=='YES'){
-      const err = new AppError("Without Providing authority to be admin i.e., 'authority': 'YES' You cannot be an admin!", 401);
-      return err.transfer(res);
+    if (authority !== 'YES') {
+        const err = new AppError("Without Providing authority to be admin i.e., 'authority': 'YES' You cannot be an admin!", 401);
+        return err.transfer(res);
     }
     const customer = await Customer.createCustomer(name, email, password, 'admin', authority);
-    if(!customer){
-      const err = new AppError('Cannot create new Admin because customer/admin with same Mail already exists!', 400);
-      return err.transfer(res);
+    if (!customer) {
+        const err = new AppError('Cannot create new Admin because customer/admin with same Mail already exists!', 400);
+        return err.transfer(res);
     }
     createSendToken(customer, 201, res);
-})
+});
--- models/Customer.js
+++ models/Customer.js
@@ -12,17 +12,15 @@ class Customer {
     }
 
     createCustomer(name, email, password, role, authority) {
-        
-        if(!this.findCustomerByEmail(email)){
+        if (!this.findCustomerByEmail(email)) {
             const customerId = uuidv4();
-            this.customers[customerId] = { name, email, password, customerId, role: role || 'customer'};
-            if(role === 'admin'){
+            this.customers[customerId] = { name, email, password, customerId, role: role || 'customer' };
+            if (role === 'admin') {
                 this.customers[customerId].authority = authority;
             }
             this.save();
             return this.customers[customerId];
-        }
-        else{
+        } else {
             return false;
         }
     }
@@ -30,18 +28,21 @@ class Customer {
     findCustomerByEmail(email) {
         return Object.values(this.customers).find(customer => customer.email === email) || null;
     }
-    findCustomerById(id){
-        if(this.customers[id])  return this.customers[id];
-        else return false;
+
+    findCustomerById(id) {
+        return this.customers[id] || null;
     }
+
     validateCustomer(email, password) {
         const customer = this.findCustomerByEmail(email);
+        console.log(customer, email, password)
         if (customer && customer.password === password) {
             return customer;
         }
         return null;
     }
-    getCustomers(){
+
+    getCustomers() {
         return this.customers;
     }
 }
