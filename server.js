5bc879e39e028fd795eba420e612df58a0af671d
Varadut1 <varaduttarwar535@gmail.com>
2024-06-14 11:01:05 +0530

    new commit

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+    console.log(`App running on port ${port}....`);
 })
